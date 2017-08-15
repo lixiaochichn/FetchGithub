815e64feeb4cb8db29d25438e1bc5ca7620ae002
lixiaochichn@gmail.com <lixiaochichn@gmail.com>
2017-08-16 00:09:46 +0800

    updata0816

---
+++ scripts/app.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    fetch('https://api.github.com/users/lixiaochichn')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (user) {
+            console.log(user);  //log user
+            return user;
+        })
+        .then(function (user) {
+            return new Promise(resolve => {
+
+                fetch(user.repos_url) //fetch user
+                    .then(function(response) {
+                        return response.json();//转成json
+                    })
+                    .then(function(repos) {
+                        user.repos = repos;//增加json user的repos
+                        resolve(user);
+                    });
+            });
+
+        })
+        .then(function(user){
+            console.log(user);
+            return user;
+        })
+        .catch(function(error){
+            console.log(error.message);
+        });
+
+
+
+
+});
