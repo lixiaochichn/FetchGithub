562395a68fef4422652dce0ef6b8f3cca4d3c87c
lixiaochichn@gmail.com <lixiaochichn@gmail.com>
2017-08-17 20:40:45 +0800

    updata0817

--- scripts/app.js
+++ scripts/app.js
@@ -5,28 +5,32 @@ document.addEventListener('DOMContentLoaded', function () {
             return response.json();
         })
         .then(function (user) {
-            console.log(user);  //log user
+            // console.log(user);  //log user
             return user;
         })
         .then(function (user) {
             return new Promise(resolve => {
 
                 fetch(user.repos_url) //fetch user
-                    .then(function(response) {
+                    .then(function (response) {
                         return response.json();//转成json
+
                     })
-                    .then(function(repos) {
+                    .then(function (repos) {
                         user.repos = repos;//增加json user的repos
                         resolve(user);
                     });
             });
 
         })
-        .then(function(user){
+        .then(function (user) {
+            console.log('q');
+            $('.usermessage').append(`<div>ID:${user.id}</div><img src="${user.avatar_url}" alt="">`);
             console.log(user);
             return user;
+
         })
-        .catch(function(error){
+        .catch(function (error) {
             console.log(error.message);
         });
 
